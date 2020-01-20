da2429e60d8e60c05012620e10c7fff09eb44593
Kasper Rynning-Tønnesen <kasper.rynning-tonnesen@knowit.no>
2020-01-20 16:45:30 +0100

    Login-fixes

--- api/login.js
+++ api/login.js
@@ -36,7 +36,7 @@ router.get("/login", function(req, res) {
 router.post(
   "/login",
   passport.authenticate("local", {
-    failureRedirect: "#/login"
+    failureRedirect: "/#/login"
   }),
   function(req, res) {
     res.redirect("/#/update");
