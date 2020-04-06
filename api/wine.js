39c4d8f13470d31b34297c46c85a0d0d7b14697f
Kasper Rynning-Tønnesen <kasper.rynning-tonnesen@knowit.no>
2020-04-06 09:36:33 +0200

    Generalized some functions

---
+++ api/wine.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const Wine = require(path.join(__dirname + "/../schemas/Wine"));
+
+async function findSaveWine(prelotteryWine) {
+  let wonWine = await Wine.findOne({ name: prelotteryWine.name });
+  if (wonWine == undefined) {
+    let newWonWine = new Wine({
+      name: prelotteryWine.name,
+      vivinoLink: prelotteryWine.vivinoLink,
+      rating: prelotteryWine.rating,
+      occurences: 1,
+      image: prelotteryWine.image,
+      id: prelotteryWine.id
+    });
+    await newWonWine.save();
+    wonWine = newWonWine;
+  } else {
+    wonWine.occurences += 1;
+    wonWine.image = prelotteryWine.image;
+    wonWine.id = prelotteryWine.id;
+    await wonWine.save();
+  }
+
+  return wonWine;
+}
+
+module.exports.findSaveWine = findSaveWine;
