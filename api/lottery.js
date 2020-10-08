9b2de7591077bab18f8c3ee9ecf88d69848dfae9
KevinMidboe <kevin.midboe@gmail.com>
2020-10-08 23:41:18 +0200

    Received and searched names are lowercased.
    Endpoint /lottery/by-name now lowercases the path input name and the
    search query towards mongo.

--- api/lottery.js
+++ api/lottery.js
@@ -98,8 +98,9 @@ const byEpochDate = (req, res) => {
 
 const byName = (req, res) => {
   const { name } = req.params;
+  const regexName = new RegExp(name, "i"); // lowercase regex of the name
 
-  return Highscore.find({ name })
+  return Highscore.find({ "name": { $regex : regexName } })
     .then(async (highscore) => {
       highscore = highscore[0]
       if (highscore) {
