9d9947d7dc0b9c61e08661583b52b7f5bc57d54e
KevinMidboe <kevin.midboe@gmail.com>
2020-10-11 13:53:08 +0200

    Router mode history, and all routes got names.

--- src/router.js
+++ src/router.js
@@ -1,3 +1,5 @@
+import VueRouter from "vue-router";
+
 import VinlottisPage from "@/components/VinlottisPage";
 import GeneratePage from "@/components/GeneratePage";
 import TodaysPage from "@/components/TodaysPage";
@@ -19,30 +21,37 @@ import AllRequestedWines from "@/components/AllRequestedWines";
 const routes = [
   {
     path: "*",
+    name: "Hjem",
     component: VinlottisPage
   },
   {
     path: "/lottery",
+    name: "Lotteri",
     component: LotteryPage
   },
   {
     path: "/dagens",
+    name: "Dagens vin",
     component: TodaysPage
   },
   {
     path: "/viner",
+    name: "Alle viner",
     component: AllWinesPage
   },
   {
     path: "/login",
+    name: "Login",
     component: LoginPage
   },
   {
     path: "/create",
+    name: "Registrer",
     component: CreatePage
   },
   {
     path: "/admin",
+    name: "Admin",
     component: AdminPage
   },
   {
@@ -54,21 +63,40 @@ const routes = [
     component: WinnerPage
   },
   {
-    path: "/history",
+    path: "/history/:date",
+    name: "Historie for dato",
     component: HistoryPage
   },
   {
+    path: "/history/",
+    name: "Historie",
+    component: HistoryPage
+  },
+  {
+    path: "/highscore/:name",
+    name: "Personlig toppliste",
+    component: PersonalHighscorePage
+  },
+  {
     path: "/highscore",
+    name: "Topplisten",
     component: HighscorePage
   },
   {
     path: "/request",
+    name: "Etterspør vin",
     component: RequestWine
   },
   {
     path: "/requested-wines",
+    name: "Etterspurte viner",
     component: AllRequestedWines
   }
 ];
 
-export { routes };
+const router = new VueRouter({
+  routes: routes,
+  mode: "history"
+});
+
+export default router;
