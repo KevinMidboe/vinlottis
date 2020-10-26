554948d67c48bb93c33bfeff435f3640f4822dc5
Kevin Midbøe <kevin.midboe@gmail.com>
2020-10-26 13:10:45 +0100

    Use new hearth icon and smaller delete btn.

--- src/ui/RequestedWineCard.vue
+++ src/ui/RequestedWineCard.vue
@@ -4,22 +4,25 @@
       <div class="flex justify-end">
         <div class="requested-count cursor-pointer" @click="request">
           <span>{{ requestedElement.count }}</span>
-          <i class="icon icon--heart">{{ locallyRequested ? "💜" : "🤍" }}</i>
+          <i class="icon icon--heart" />
         </div>
       </div>
     </template>
 
     <template v-slot:default>
-      <button @click="deleteWine(wine)" v-if="showDeleteButton == true" class="vin-button small danger">
+      <button @click="deleteWine(wine)" v-if="showDeleteButton == true" class="vin-button small danger width-100">
         Slett vinen
       </button>
     </template>
 
     <template v-slot:bottom>
-      <a aria-role="button" tabindex="0" class="link float-left" @click="request"
-         :class="{ 'active': locallyRequested }">
-        Anbefal
-      </a>
+      <div class="float-left request">
+        <i class="icon icon--heart request-icon"></i>
+        <a aria-role="button" tabindex="0" class="link" @click="request"
+           :class="{ 'active': locallyRequested }">
+          Anbefal
+        </a>
+      </div>
     </template>
   </Wine>
 </template>
@@ -77,26 +80,40 @@ export default {
 @import "./src/styles/variables";
 
 .requested-count {
-  display: inline-block;
+  display: flex;
+  align-items: center;
   margin-top: -0.5rem;
   background-color: rgb(244,244,244);
   border-radius: 1.1rem;
-  font-size: 1.1rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
+  padding: 0.25rem 1rem;
+  font-size: 1.25em;
 
   span {
-    padding: 0.6rem 0;
-    padding-right: 0.25rem;
-    display: inline-block;
+    padding-right: 0.5rem;
+    line-height: 1.25em;
   }
 
-  .icon {
-    font-style: unset;
+  .icon--heart{
+    color: $link-color;
   }
 }
 
 .link .active {
   border-color: $link-color;
+}
+
+
+.request {
+  display: flex;
+  align-items: center;
+
+  &-icon {
+    font-size: 2rem;
+    color: $link-color;
+  }
+
+  a {
+    margin-left: 0.75rem;
+  }
 }
 </style>
