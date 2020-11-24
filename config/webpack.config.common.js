7c0d7c14ec7ec11d3f836fb3f5ac483e17713b41
KevinMidboe <kevin.midboe@gmail.com>
2020-11-24 23:45:33 +0100

    Urlloader uses options not query params.

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -64,7 +64,11 @@ const webpackConfig = function(isDev) {
         },
         {
           test: /\.woff(2)?(\?[a-z0-9]+)?$/,
-          loader: "url-loader?limit=10000&mimetype=application/font-woff"
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff"
+          }
         },
         {
           test: /\.(ttf|eot|svg)(\?[a-z0-9]+)?$/,
