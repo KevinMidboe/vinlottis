53780878af4a34e45dd89752f2bce667fbfb1984
KevinMidboe <kevin.midboe@gmail.com>
2021-01-17 16:55:57 +0100

    Renamed to winners. Winners gets controller setup.
    Rewrote everything that happened in history to better take advantage of
    monogdb instead of doing everything in js.
    
    Our endpoints become:
     - /winners - getAll w/ includeWines and sort query params.
     - /winners/latest - latest winners grouped w/ includeWines query
     params.
     - /winners/by-date - all winners grouped by date w/ includeWines and
     sort.
     - /winners/by-date/:date - get winners per epoch or string date.
     - /winners/by-name/:name - get winner by name parameter w/ sort for
     wins direction.

---
+++ api/controllers/winnerController.js
@@ -0,0 +1,165 @@
+const path = require("path");
+const winnerRepository = require(path.join(__dirname, "../winner"));
+
+const sortOptions = ["desc", "asc"];
+const includeWinesOptions = ["true", "false"];
+
+const all = (req, res) => {
+  let { sort, includeWines } = req.query;
+
+  if (sort !== undefined && !sortOptions.includes(sort)) {
+    return res.status(400).send({
+      message: `Sort option must be: '${sortOptions.join(", ")}'`,
+      success: false
+    });
+  }
+
+  if (includeWines !== undefined && !includeWinesOptions.includes(includeWines)) {
+    return res.status(400).send({
+      message: `includeWines option must be: '${includeWinesOptions.join(", ")}'`,
+      success: false
+    });
+  }
+
+  return winnerRepository
+    .all(includeWines == "true")
+    .then(winners =>
+      res.send({
+        winners: sort !== "asc" ? winners : winners.reverse(),
+        success: true
+      })
+    )
+    .catch(error => {
+      const { statusCode, message } = error;
+
+      return res.status(statusCode || 500).send({
+        success: false,
+        message: message || "Unable to fetch winners."
+      });
+    });
+};
+
+const byDate = (req, res) => {
+  let { date } = req.params;
+
+  const regexDate = new RegExp("^\\d{4}-\\d{2}-\\d{2}$");
+  if (!isNaN(date)) {
+    date = new Date(new Date(parseInt(date * 1000)).setHours(0, 0, 0, 0));
+  } else if (regexDate.test(date)) {
+    date = new Date(date);
+  } else if (date !== undefined) {
+    return res.status(400).send({
+      message: "Invalid date parameter, allowed epoch seconds or YYYY-MM-DD.",
+      success: false
+    });
+  }
+
+  return winnerRepository
+    .byDate(date)
+    .then(winners =>
+      res.send({
+        date: date,
+        winners: winners,
+        success: true
+      })
+    )
+    .catch(error => {
+      const { statusCode, message } = error;
+
+      return res.status(statusCode || 500).send({
+        success: false,
+        message: message || "Unable to fetch winner by date."
+      });
+    });
+};
+
+const groupedByDate = (req, res) => {
+  let { sort, includeWines } = req.query;
+
+  if (sort !== undefined && !sortOptions.includes(sort)) {
+    return res.status(400).send({
+      message: `Sort option must be: '${sortOptions.join(", ")}'`,
+      success: false
+    });
+  }
+
+  if (includeWines !== undefined && !includeWinesOptions.includes(includeWines)) {
+    return res.status(400).send({
+      message: `includeWines option must be: '${includeWinesOptions.join(", ")}'`,
+      success: false
+    });
+  }
+
+  return winnerRepository
+    .groupedByDate(includeWines, sort)
+    .then(lotteries =>
+      res.send({
+        lotteries: lotteries,
+        success: true
+      })
+    )
+    .catch(error => {
+      const { statusCode, message } = error;
+
+      return res.status(statusCode || 500).send({
+        success: false,
+        message: message || "Unable to fetch winner by date."
+      });
+    });
+};
+
+const latest = (req, res) => {
+  return winnerRepository
+    .latest()
+    .then(winners =>
+      res.send({
+        ...winners,
+        success: true
+      })
+    )
+    .catch(error => {
+      const { statusCode, message } = error;
+
+      return res.status(statusCode || 500).send({
+        success: false,
+        message: message || "Unable to fetch winner by date."
+      });
+    });
+};
+
+const byName = (req, res) => {
+  const { name } = req.params;
+  const { sort } = req.query;
+
+  if (sort !== undefined && !sortOptions.includes(sort)) {
+    return res.status(400).send({
+      message: `Sort option must be: '${sortOptions.join(", ")}'`,
+      success: false
+    });
+  }
+
+  return winnerRepository
+    .byName(name, sort)
+    .then(winner =>
+      res.send({
+        winner: winner,
+        success: true
+      })
+    )
+    .catch(error => {
+      const { statusCode, message } = error;
+
+      return res.status(statusCode || 500).send({
+        success: false,
+        message: message || "Unable to fetch winner by name."
+      });
+    });
+};
+
+module.exports = {
+  all,
+  byDate,
+  groupedByDate,
+  latest,
+  byName
+};
---
+++ api/winner.js
@@ -0,0 +1,191 @@
+const path = require("path");
+
+const Winner = require(path.join(__dirname, "/schemas/Highscore"));
+const Wine = require(path.join(__dirname, "/schemas/Wine"));
+
+class HistoryByDateNotFound extends Error {
+  constructor(message = "History for given date not found.") {
+    super(message);
+    this.name = "HistoryByDateNotFound";
+    this.statusCode = 404;
+  }
+}
+
+class HistoryForUserNotFound extends Error {
+  constructor(message = "History for given user not found.") {
+    super(message);
+    this.name = "HistoryForUserNotFound";
+    this.statusCode = 404;
+  }
+}
+
+const all = (includeWines = false) => {
+  if (includeWines === false) {
+    return Winner.find().sort("-wins.date");
+  } else {
+    return Winner.find()
+      .sort("-wins.date")
+      .populate("wins.wine");
+  }
+};
+
+const byDate = date => {
+  const startQueryDate = new Date(date.setHours(0, 0, 0, 0));
+  const endQueryDate = new Date(date.setHours(24, 59, 59, 99));
+  const query = [
+    {
+      $match: {
+        "wins.date": {
+          $gte: startQueryDate,
+          $lte: endQueryDate
+        }
+      }
+    },
+    { $unwind: "$wins" },
+    {
+      $match: {
+        "wins.date": {
+          $gte: startQueryDate,
+          $lte: endQueryDate
+        }
+      }
+    },
+    {
+      $lookup: {
+        from: "wines",
+        localField: "wins.wine",
+        foreignField: "_id",
+        as: "wins.wine"
+      }
+    },
+    { $unwind: "$wins.wine" },
+    {
+      $project: {
+        name: "$name",
+        date: "$wins.date",
+        color: "$wins.color",
+        wine: "$wins.wine"
+      }
+    }
+  ];
+
+  return Winner.aggregate(query).then(winners => {
+    if (winners.length == 0) {
+      throw new HistoryByDateNotFound();
+    }
+    return winners;
+  });
+};
+
+const byName = (name, sort = "desc") => {
+  const populateOptions = { sort: "date" };
+
+  return Winner.findOne({ name }, ["name", "wins"])
+    .sort("-wins.date")
+    .populate("wins.wine")
+    .then(winner => {
+      if (winner) {
+        winner.wins = sort !== "asc" ? winner.wins.reverse() : winner.wins;
+        return winner;
+      } else {
+        throw new HistoryForUserNotFound();
+      }
+    });
+};
+
+const latest = () => {
+  const query = [
+    {
+      $unwind: "$wins"
+    },
+    {
+      $lookup: {
+        from: "wines",
+        localField: "wins.wine",
+        foreignField: "_id",
+        as: "wins.wine"
+      }
+    },
+    {
+      $group: {
+        _id: "$wins.date",
+        winners: {
+          $push: {
+            _id: "$_id",
+            name: "$name",
+            color: "$wins.color",
+            wine: "$wins.wine"
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        date: "$_id",
+        winners: "$winners"
+      }
+    },
+    {
+      $sort: {
+        _id: -1
+      }
+    },
+    {
+      $limit: 1
+    }
+  ];
+
+  return Winner.aggregate(query).then(winners => winners[0]);
+};
+
+const groupedByDate = (includeWines = false, sort = "desc") => {
+  const query = [
+    {
+      $unwind: "$wins"
+    },
+    {
+      $group: {
+        _id: "$wins.date",
+        winners: {
+          $push: {
+            _id: "$_id",
+            name: "$name",
+            color: "$wins.color",
+            wine: "$wins.wine"
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        date: "$_id",
+        winners: "$winners"
+      }
+    },
+    {
+      $sort: {
+        _id: -1
+      }
+    }
+  ];
+
+  if (includeWines) {
+    query.splice(1, 0, {
+      $lookup: {
+        from: "wines",
+        localField: "wins.wine",
+        foreignField: "_id",
+        as: "wins.wine"
+      }
+    });
+  }
+
+  return Winner.aggregate(query).then(lotteries => (sort != "asc" ? lotteries : lotteries.reverse()));
+};
+
+module.exports = {
+  all,
+  byDate,
+  latest,
+  groupedByDate
+};
