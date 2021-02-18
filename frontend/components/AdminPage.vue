b57fb5f1f81bd3aebb5e364f21099b176d4251e6
KevinMidboe <kevin.midboe@gmail.com>
2021-02-18 21:08:37 +0100

    Admin gets new tabs.
    Tabs for registering wines, adding attendees, drawing winner,
    registering/archiving lottery and sending push notifications.
    Each tab also has a slug and counter attribute.

--- frontend/components/AdminPage.vue
+++ frontend/components/AdminPage.vue
@@ -1,34 +1,75 @@
 <template>
   <div>
-    <h1>Admin-side</h1>
     <Tabs :tabs="tabs" />
   </div>
 </template>
 
 <script>
 import Tabs from "@/ui/Tabs";
+import RegisterWinePage from "@/components/admin/RegisterWinePage";
 import RegisterPage from "@/components/RegisterPage";
 import VirtualLotteryRegistrationPage from "@/components/VirtualLotteryRegistrationPage";
+import DrawWinnerPage from "@/components/admin/DrawWinnerPage";
+import PushPage from "@/components/admin/PushPage";
 
 export default {
   components: {
     Tabs,
     RegisterPage,
-    VirtualLotteryRegistrationPage
+    VirtualLotteryRegistrationPage,
+    DrawWinnerPage
   },
   data() {
     return {
       tabs: [
-        { name: "Registrering", component: RegisterPage },
-        { name: "Virtuelt lotteri", component: VirtualLotteryRegistrationPage }
+        {
+          name: "Vin",
+          component: RegisterWinePage,
+          slug: "vin",
+          counter: null
+        },
+        {
+          name: "Legg til deltakere",
+          component: VirtualLotteryRegistrationPage,
+          slug: "attendees",
+          counter: null
+        },
+        {
+          name: "Trekk vinner",
+          component: DrawWinnerPage,
+          slug: "draw",
+          counter: null
+        },
+        {
+          name: "Arkiver lotteri",
+          component: RegisterPage,
+          slug: "reg",
+          counter: null
+        },
+        {
+          name: "Push meldinger",
+          component: PushPage,
+          slug: "push"
+        }
       ]
     };
   }
 };
 </script>
 
-<style lang="scss" scoped>
-h1 {
-  text-align: center;
+<style lang="scss">
+@import "@/styles/media-queries";
+
+.page-container {
+  padding: 0 1.5rem 3rem;
+
+  h1 {
+    text-align: center;
+  }
+
+  @include desktop {
+    max-width: 60vw;
+    margin: 0 auto;
+  }
 }
 </style>
